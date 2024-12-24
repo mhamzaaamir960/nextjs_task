6b18491f7906a453132faa5e4138c4c59152a0ac
mhamzaaamir960 <mhamzaaamir960@gmail.com>
2024-12-24 23:05:23 +0500

    use material ui Button component

--- src/components/AddContent/AddContent.tsx
+++ src/components/AddContent/AddContent.tsx
@@ -1,48 +1,63 @@
+"use client";
 import React from "react";
 import { RxVideo } from "react-icons/rx";
 import { IoImageOutline, IoDocumentTextOutline } from "react-icons/io5";
 import { HiOutlineSpeakerWave } from "react-icons/hi2";
 import { MdOutlineTextFields } from "react-icons/md";
 import { RiLinksFill } from "react-icons/ri";
+import { Button } from "@mui/material";
 
 function AddContent() {
+  const buttons = [
+    { icon: <RxVideo />, label: "Video" },
+    { icon: <IoImageOutline />, label: "Image" },
+    { icon: <HiOutlineSpeakerWave />, label: "Audio" },
+    { icon: <IoDocumentTextOutline />, label: "Document" },
+    { icon: <MdOutlineTextFields />, label: "Text" },
+    { icon: <RiLinksFill />, label: "Resources" },
+  ];
+
   return (
-    <div className=" md:w-[400px] lg:w-[600px] w-full h-fit  bg-white border border-gray-300 rounded-xl  order-first md:order-last mt-10 md:mt-0">
+    <div className="md:w-[400px] lg:w-[600px] w-full h-fit bg-white border border-gray-300 rounded-xl order-first md:order-last mt-10 md:mt-0">
       <div className="h-[60px] text-black font-medium text-xl sm:text-2xl flex items-center border-b border-gray-300 pl-5">
-        Add content{" "}
+        Add content
       </div>
       <div className="w-full min-h-fit bg-transparent rounded-b-xl p-5">
-        <div className="w-full h-fit  text-black font-medium text-xl flex flex-wrap justify-center items-center  gap-5">
-          {/* for video */}
-          <button className="w-[90px] h-[70px] lg:w-[110px] lg:h-[90px] xl:w-[150px] xl:h-[120px] bg-white hover:bg-gray-50  cursor-pointer flex  flex-col justify-center items-center border boder-gray-500 rounded-xl ">
-            <RxVideo className="text-blue-500 text-[28px] lg:text-[34px] " />
-            <p className="text-[16px] lg:text-[18px] text-gray-700">Video</p>
-          </button>
-          {/* for image */}
-          <button className="w-[90px] h-[70px] lg:w-[110px] lg:h-[90px] xl:w-[150px] xl:h-[120px] bg-white hover:bg-gray-50  cursor-pointer flex  flex-col justify-center items-center border boder-gray-500 rounded-xl">
-            <IoImageOutline className="text-blue-500 text-[28px] lg:text-[34px] " />
-            <p className="text-[16px] lg:text-[18px] text-gray-700">Image</p>
-          </button>
-          {/* for image */}
-          <button className="w-[90px] h-[70px] lg:w-[110px] lg:h-[90px] xl:w-[150px] xl:h-[120px] bg-white hover:bg-gray-50  cursor-pointer flex  flex-col justify-center items-center border boder-gray-500 rounded-xl">
-            <HiOutlineSpeakerWave className="text-blue-500 text-[28px] lg:text-[34px] " />
-            <p className=" text-[16px] lg:text-[18px] text-gray-700">Audio</p>
-          </button>
-          {/* for image */}
-          <button className="w-[90px] h-[70px] lg:w-[110px] lg:h-[90px] xl:w-[150px] xl:h-[120px] bg-white hover:bg-gray-50  cursor-pointer flex  flex-col justify-center items-center border boder-gray-500 rounded-xl">
-            <IoDocumentTextOutline className="text-blue-500 text-[28px] lg:text-[34px] " />
-            <p className="text-[16px] lg:text-[18px] text-gray-700">Document</p>
-          </button>
-          {/* for image */}
-          <button className="w-[90px] h-[70px] lg:w-[110px] lg:h-[90px] xl:w-[150px] xl:h-[120px] bg-white hover:bg-gray-50  cursor-pointer flex  flex-col justify-center items-center border boder-gray-500 rounded-xl">
-            <MdOutlineTextFields className="text-blue-500 text-[28px] lg:text-[34px] " />
-            <p className="text-[16px] lg:text-[18px] text-gray-700">Text</p>
-          </button>
-          {/* for image */}
-          <button className="w-[90px] h-[70px] lg:w-[110px] lg:h-[90px] xl:w-[150px] xl:h-[120px] bg-white hover:bg-gray-50  cursor-pointer flex  flex-col justify-center items-center border boder-gray-500 rounded-xl">
-            <RiLinksFill className="text-blue-500 text-[28px] lg:text-[34px] " />
-            <p className="text-[16px] lg:text-[18px] text-gray-700">Resources</p>
-          </button>
+        <div className="w-full h-fit text-black font-medium text-xl flex flex-wrap justify-center items-center gap-5">
+          {buttons.map((button, index) => (
+            <Button
+              key={index}
+              variant="outlined"
+              sx={{
+                width: { xs: "90px", lg: "110px", xl: "150px" },
+                height: { xs: "70px", lg: "90px", xl: "120px" },
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                border: "1px solid #d1d5db",
+                backgroundColor: "white",
+                "&:hover": {
+                  backgroundColor: "gray.100",
+                },
+                borderRadius: "12px",
+              }}
+            >
+              <span style={{ color: "#3b82f6", fontSize: "2em" }}>
+                {button.icon}
+              </span>
+
+              <span
+                style={{
+                  fontSize: "16px",
+                  color: "#4b5563",
+                  textTransform: "capitalize",
+                }}
+              >
+                {button.label}
+              </span>
+            </Button>
+          ))}
         </div>
       </div>
     </div>
